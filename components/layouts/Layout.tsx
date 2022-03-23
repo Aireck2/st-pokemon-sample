98012396c7a1ba621925c6c315a3d6f940114a2d
@erick <erick.hx22@gmail.com>
2022-03-22 20:18:28 -0500

    chore: implement open graph meta tags (#6)

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -5,6 +5,9 @@ import { Navbar } from "../ui";
 interface LayoutProps {
   title?: string;
 }
+
+const origin = typeof window === "undefined" ? null : window.location.origin;
+
 export const Layout: FC<LayoutProps> = ({ children, title }) => (
   <>
     <Head>
@@ -15,6 +18,13 @@ export const Layout: FC<LayoutProps> = ({ children, title }) => (
         content={`Information about pokemon, ${title}`}
       />
       <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+      <meta property="og:title" content={title || "Pokemon list"} />
+      <meta
+        property="og:description"
+        content={`Information about pokemon, ${title}`}
+      />
+      <meta property="og:image" content={`${origin}/img/banner.png`} />
     </Head>
 
     <Navbar />
