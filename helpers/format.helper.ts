f402703cca1283614f03c53341ff8e5cf8f82dc9
@erick <erick.hx22@gmail.com>
2022-03-23 13:38:40 -0500

    refactor: implement try catch in getformatpokemon (#7)

--- helpers/format.helper.ts
+++ helpers/format.helper.ts
@@ -3,19 +3,23 @@ import { pokeApi } from "../api";
 
 export const getFormatPokemon = async (
   nameOrId: string
-): Promise<DetailPokemon> => {
-  const { data: pokemon } = await pokeApi.get<PokemonFull>(
-    `pokemon/${nameOrId}`
-  );
+): Promise<DetailPokemon | null> => {
+  try {
+    const { data: pokemon } = await pokeApi.get<PokemonFull>(
+      `pokemon/${nameOrId}`
+    );
 
-  return {
-    id: pokemon?.id,
-    name: pokemon?.name,
-    mainImage:
-      pokemon?.sprites?.other?.dream_world.front_default || "no-image.png",
-    frontImage: pokemon?.sprites?.front_default,
-    backImage: pokemon?.sprites?.back_default,
-    frontShinyImage: pokemon?.sprites?.front_shiny,
-    backShinyImage: pokemon?.sprites?.back_shiny,
-  };
+    return {
+      id: pokemon?.id,
+      name: pokemon?.name,
+      mainImage:
+        pokemon?.sprites?.other?.dream_world.front_default || "no-image.png",
+      frontImage: pokemon?.sprites?.front_default,
+      backImage: pokemon?.sprites?.back_default,
+      frontShinyImage: pokemon?.sprites?.front_shiny,
+      backShinyImage: pokemon?.sprites?.back_shiny,
+    };
+  } catch (err) {
+    return null;
+  }
 };
