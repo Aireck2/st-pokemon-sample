acd020138fca712984d0ae153fd0e9a415f0e5fb
@erick <erick.hx22@gmail.com>
2022-11-07 12:12:17 -0500

    feat: update ui (#8)

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import Head from "next/head";
-import { Navbar } from "../ui";
+import { Header } from "../ui";
+
+import { capitalize } from "@/helpers/functions";
 
 interface LayoutProps {
   title?: string;
@@ -8,10 +10,13 @@ interface LayoutProps {
 
 const origin = typeof window === "undefined" ? null : window.location.origin;
 
-export const Layout: FC<LayoutProps> = ({ children, title }) => (
+export const Layout: FC<LayoutProps & { children: React.ReactNode }> = ({
+  children,
+  title,
+}) => (
   <>
     <Head>
-      <title>{title || "Pokemon App"}</title>
+      <title>{`${capitalize(title)} | Pokemon` || "Pokemon App"}</title>
       <meta name="author" content="Erick Escriba" />
       <meta
         name="description"
@@ -24,10 +29,10 @@ export const Layout: FC<LayoutProps> = ({ children, title }) => (
         property="og:description"
         content={`Information about pokemon, ${title}`}
       />
-      <meta property="og:image" content={`${origin}/img/banner.png`} />
+      <meta property="og:image" content={`${origin}/img/banner-min.png`} />
     </Head>
 
-    <Navbar />
+    <Header />
     <main style={{ padding: 20 }}>{children}</main>
   </>
 );
