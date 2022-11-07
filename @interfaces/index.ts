507334a9c044e4e5da686cd6de0d81eb7527ac27
@erick <erick.hx22@gmail.com>
2022-11-07 12:11:19 -0500

    refactor: rename interfaces to models (#8)

--- @interfaces/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./pokemons.interface";
-export * from "./pokemon-full.interface";
-export * from "./pages.interface";
---
+++ models/index.ts
@@ -0,0 +1,2 @@
+export * from './pages.interface';
+export * from './pokemon';
